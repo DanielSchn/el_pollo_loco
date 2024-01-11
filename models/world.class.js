22cd1ef1406bb9252e0912c48fddfd7172052d39
DanielSchn <d.schneider@dsc-mail.de>
2024-01-11 11:34:37 +0100

    redraw function

--- models/world.class.js
+++ models/world.class.js
@@ -5,17 +5,26 @@ class World {
         new Chicken(),
         new Chicken()
     ];
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        for (let i = 0; i < this.enemies.length; i++) {
-            this.ctx.drawImage(this.enemies[i].img, this.enemies[i].x, this.enemies[i].y, 100, 200);    
-        };
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height); 
+        });
+
+        let self = this;        // Problem der OOP, dass man ein this nicht innerhalb dieser Funktion nutzen kann. Deshalb wird hier einfach eine Hilfsvariable festgelegt.
+        requestAnimationFrame(function() {
+            self.draw();
+        });
     }
 }
