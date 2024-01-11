7fdc1ccac3976d9da1d2e2f878bab01e01af81bf
DanielSchn <d.schneider@dsc-mail.de>
2024-01-11 14:32:53 +0100

    log for dev

--- js/game.js
+++ js/game.js
@@ -7,5 +7,6 @@ function init() {
     world = new World(canvas);
 
     console.log('My Character is', world.character);
-    console.log('My Character is', world.enemies);
+    console.log('My Chicken is', world.enemies);
+    console.log('My Cloud is', world.clouds);
 }
