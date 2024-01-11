2c8762f682aa70da669cd71d948e7e721671d51e
DanielSchn <d.schneider@dsc-mail.de>
2024-01-11 12:29:17 +0100

    optic

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject {
-
+    height = 300;
+    width = 152;
+    y = 120;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MovableObject {
-
+    height = 60;
+    width = 62;
+    y = 350;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
