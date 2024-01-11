a077a70301268ff045ab8e9392cfb6b963e554ff
DanielSchn <d.schneider@dsc-mail.de>
2024-01-11 12:07:36 +0100

    add clouds

---
+++ models/cloud.class.js
@@ -0,0 +1,11 @@
+class Cloud extends MovableObject {
+    y = 0;
+    height = 270;
+    width = 480;
+    
+    
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+        this.x = Math.random() * 500;
+    }
+}
