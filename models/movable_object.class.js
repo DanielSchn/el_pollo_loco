396fb8dcf4d7cb972b67f4aab1518af5e728f499
DanielSchn <d.schneider@dsc-mail.de>
2024-01-12 16:29:34 +0100

    add imageCache

--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -4,11 +4,25 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
 
     loadImage(path) {
         this.img = new Image(); // same as <img>
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
+
     }
 
     moveRight() {
