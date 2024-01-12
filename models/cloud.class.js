1ef99a87730c369407f5a2577ab18103872584d0
DanielSchn <d.schneider@dsc-mail.de>
2024-01-12 12:34:36 +0100

    add cloud animation

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -6,6 +6,16 @@ class Cloud extends MovableObject {
     
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.x = Math.random() * 500;
+        // this.x = Math.random() * 500;
+        this.x = 680;
+        this.animateCloud();
+
+    }
+
+    animateCloud() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
+        
     }
 }
