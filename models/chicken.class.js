24c110c279ab2a2a4453e9fc562dc2ad21731733
DanielSchn <d.schneider@dsc-mail.de>
2024-01-12 16:51:35 +0100

    chicken animation

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,7 +3,6 @@ class Chicken extends MovableObject {
     width = 62;
     y = 375;
     IMAGES_CHICKEN = [
-        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
@@ -11,7 +10,8 @@ class Chicken extends MovableObject {
     currentImage = 0;
 
     constructor() {
-        super().loadImage(this.IMAGES_CHICKEN[0]);
+        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.loadImages(this.IMAGES_CHICKEN);
         this.animateChicken();
         this.x = 200 + Math.random() * 500;
     }
@@ -19,8 +19,10 @@ class Chicken extends MovableObject {
     animateChicken() {
         setInterval(() => {
             this.x -= 2;
-            this.currentImage = (this.currentImage + 1) % this.IMAGES_CHICKEN.length;
-            this.loadImage(this.IMAGES_CHICKEN[this.currentImage]);
+            let i = this.currentImage % this.IMAGES_CHICKEN.length;
+            let path = this.IMAGES_CHICKEN[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
         }, 1000);
     }
 
