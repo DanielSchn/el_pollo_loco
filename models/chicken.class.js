3e3470afb2c8aa2cd67be6fe9b720e67b7a79a52
DanielSchn <d.schneider@dsc-mail.de>
2024-01-12 12:56:11 +0100

    animate chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,10 +2,25 @@ class Chicken extends MovableObject {
     height = 60;
     width = 62;
     y = 375;
+    imagePaths = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
+    currentPathIndex = 0;
 
     constructor() {
-        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-
+        super().loadImage(this.imagePaths[0]);
+        this.animateChicken();
         this.x = 200 + Math.random() * 500;
     }
+
+    animateChicken() {
+        setInterval(() => {
+            this.x -= 2;
+            this.currentPathIndex = (this.currentPathIndex + 1) % this.imagePaths.length;
+            this.loadImage(this.imagePaths[this.currentPathIndex]);
+        }, 1000);
+    }
+
 }
