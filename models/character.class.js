032113be731a2be351f860ba467204d92ad0e0f0
DanielSchn <d.schneider@dsc-mail.de>
2024-01-12 11:39:08 +0100

    add background layer

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
     height = 300;
     width = 152;
-    y = 120;
+    y = 145;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
