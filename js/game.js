bd1c7d529238cbebf3f2e714c7c6158e5c6b98c9
DanielSchn <d.schneider@dsc-mail.de>
2024-01-15 12:11:53 +0100

    add keyboard eventlistener

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 let canvas;
 let world;
-
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
@@ -10,3 +10,8 @@ function init() {
     console.log('My Chicken is', world.enemies);
     console.log('My Cloud is', world.clouds);
 }
+
+
+window.addEventListener('keypress', (e) => {
+    console.log(e);
+});
