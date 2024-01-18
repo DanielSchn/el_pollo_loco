c0c8fce07ac798c488db7ca33bc74a95744d1ec6
DanielSchn <d.schneider@dsc-mail.de>
2024-01-18 12:07:49 +0100

    add baychicken

--- levels/level1.js
+++ levels/level1.js
@@ -5,11 +5,11 @@ const level1 = new Level(
         new Chicken(),
         new Chicken(),
         new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
+        new BabyChicken(),
+        new BabyChicken(),
+        new BabyChicken(),
+        new BabyChicken(),
+        new BabyChicken(),
         new Endboss()
     ],
     [
---
+++ models/babychicken.class.js
@@ -0,0 +1,28 @@
+class BabyChicken extends MovableObject {
+    height = 50;
+    width = 52;
+    y = 380;
+
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+
+    constructor() {
+        super().loadImage(this.IMAGES_WALKING[1]);
+        this.loadImages(this.IMAGES_WALKING);
+        this.x = 500 + Math.random() * 1500;
+        this.speed = 0.05 + Math.random();
+        this.animateChicken();
+        
+    }
+
+    animateChicken() {
+        this.moveLeft();
+
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
+    }
+}
