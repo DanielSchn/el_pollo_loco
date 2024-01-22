64a72f2f360d7404f7763757e37ae9e3d7ee5e8a
DanielSchn <d.schneider@dsc-mail.de>
2024-01-22 12:23:54 +0100

    add statusbar

---
+++ models/drawable_object.class.js
@@ -0,0 +1,33 @@
+class DrawableObject {
+    x = 120;
+    y = 250;
+    img;
+    height = 150;
+    width = 100;
+    imageCache = {};
+    currentImage = 0;
+
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+}
--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 250;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -25,9 +18,6 @@ class MovableObject {
         }, 1000 / 60);
     }
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
 
     drawFrame(ctx) {
         if (this instanceof Character || this instanceof Chicken || this instanceof BabyChicken) {
@@ -39,6 +29,7 @@ class MovableObject {
         }
     }
 
+
     hit() {
         this.energy -= 5;
         if (this.energy < 0) {
@@ -48,41 +39,29 @@ class MovableObject {
         }
     }
 
+
     isHurt() {
         let timepassed = new Date().getTime() - this.lasHit;
         timepassed = timepassed / 1000;
         return timepassed < 1;
     }
 
+
     isDead() {
         return this.energy == 0;
     }
 
+
     isAboveGround() {
         return this.y < 132;
     }
+
 
     isColliding(mo) {
         return (this.x + this.width - this.offsetX) >= mo.x && this.x <= (mo.x + mo.width - this.offsetX) &&
             (this.y + this.offsetY + this.height) >= mo.y && (this.y + this.offsetY) <= (mo.y + mo.height);
     }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    /**
-     * 
-     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
 
     playAnimation(images) {
         let i = this.currentImage % images.length;
@@ -91,15 +70,18 @@ class MovableObject {
         this.currentImage++;
     }
 
+
     moveRight() {
         this.x += this.speed;
         this.otherDirection = false;
     }
 
+
     moveLeft(bool) {
         this.x -= this.speed;
         this.otherDirection = bool;
     }
+
 
     jump() {
         this.speedY = 20;
