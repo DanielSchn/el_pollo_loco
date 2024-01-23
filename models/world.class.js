dca70669c23eeebb6d8b734f4713f77d28e68fe3
DanielSchn <d.schneider@dsc-mail.de>
2024-01-23 16:22:48 +0100

    add bottle and coin collision

--- models/world.class.js
+++ models/world.class.js
@@ -7,6 +7,8 @@ class World {
     camera_x;
     statusBar = new StatusBar();
     throwableObjects = [];
+    collectedBottles = 0;
+    collectedCoins = 0;
 
 
     constructor(canvas, keyboard) {
@@ -37,6 +39,8 @@ class World {
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.throwableObjects);
+        this.addObjectsToMap(this.level.bottles);
+        this.addObjectsToMap(this.level.coins);
 
         this.ctx.translate(-this.camera_x, 0);
 
@@ -47,11 +51,39 @@ class World {
     }
 
     checkCollision() {
+        this.checkEnemieCollision();
+        this.checkBottleCollision();
+        this.checkCoinCollision();
+    }
+
+    checkEnemieCollision() {
         this.level.enemies.forEach((enemy) => {
             if (this.character.isColliding(enemy)) {
                 this.character.hit();
                 this.statusBar.setPercentage(this.character.energy);
             }
+        });
+    }
+
+    checkBottleCollision() {
+        this.level.bottles = this.level.bottles.filter((bottle) => {
+            if (this.character.isColliding(bottle)) {
+                this.collectedBottles++;
+                console.log('collectedBottles', this.collectedBottles);
+                return false;
+            }
+            return true;
+        });
+    }
+
+    checkCoinCollision() {
+        this.level.coins = this.level.coins.filter((coin) => {
+            if (this.character.isColliding(coin)) {
+                this.collectedCoins++;
+                console.log('collectedCoins', this.collectedCoins);
+                return false;
+            }
+            return true;
         });
     }
 
@@ -61,11 +93,12 @@ class World {
             this.checkThrowObject();
         }, 200);
     }
-    
+
     checkThrowObject() {
-        if (this.keyboard.THROW) {
+        if (this.keyboard.THROW && this.collectedBottles > 0) {
             let bottle = new ThrowableObject(this.character.x + 70, this.character.y + 150);
             this.throwableObjects.push(bottle);
+            this.collectedBottles--;
         }
     }
 
