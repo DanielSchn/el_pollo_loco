e2ee42fa78d26e8952f9cde6b107729c3c6d00e5
DanielSchn <d.schneider@dsc-mail.de>
2024-01-26 16:34:42 +0100

    add endboss animation to attack

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -27,27 +27,49 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/5_dead/G26.png'
     ];
 
+    IMAGES_ATTACK = [
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png'
+    ];
+
     constructor() {
         super().loadImage(this.IMAGES_WALKING[1]);
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_HURT);
         this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_ATTACK);
         this.animateEndboss();
     }
-
+    hadFirstContact = false;
     animateEndboss() {
         let animationPlayed = 0;
+        let i = 0;
         setInterval(() => {
             if (this.isDead()) {
-                if (animationPlayed < 30){
-                this.playAnimation(this.IMAGES_DEAD);
-                animationPlayed++;
+                if (animationPlayed < 30) {
+                    this.playAnimation(this.IMAGES_DEAD);
+                    animationPlayed++;
                 }
             } else if (this.isHurt()) {
                 this.playAnimation(this.IMAGES_HURT);
                 animationPlayed = 0;
             } else {
-                this.playAnimation(this.IMAGES_WALKING);
+                if (i < 20) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                } else {
+                    this.playAnimation(this.IMAGES_ATTACK);
+                }
+                i++;
+                if (world.character.x > 3300 && !this.hadFirstContact) {
+                    i = 0;
+                    this.hadFirstContact = true;
+                }
                 animationPlayed = 0;
             }
         }, 100);
