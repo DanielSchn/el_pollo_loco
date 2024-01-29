847fc8fb10278e640687f3d5a3569f41943f226d
DanielSchn <d.schneider@dsc-mail.de>
2024-01-29 15:55:18 +0100

    audio + fullscreen

--- js/game.js
+++ js/game.js
@@ -8,6 +8,9 @@ function init() {
     document.getElementById('helpButton').classList.add('d-none');
     document.getElementById('startButton').classList.add('d-none');
     document.getElementById('soundButtonOn').classList.remove('d-none');
+    document.getElementById('resetGame').classList.remove('d-none');
+    document.getElementById('buttons').style.justifyContent = 'center';
+    document.getElementById('buttons').style.gap = '32px';
 }
 
 function startImage() {
@@ -21,4 +24,14 @@ function startImage() {
 
 function showHelp() {
     document.getElementById('instructions').classList.toggle('d-flex');
+}
+
+function goFullScreen(){
+    var canvas = document.getElementById('canvas');
+    if(canvas.requestFullScreen)
+        canvas.requestFullScreen();
+    else if(canvas.webkitRequestFullScreen)
+        canvas.webkitRequestFullScreen();
+    else if(canvas.mozRequestFullScreen)
+        canvas.mozRequestFullScreen();
 }
