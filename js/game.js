002585bfe0ee483426fb5dafdd0e1816c4651eea
DanielSchn <d.schneider@dsc-mail.de>
2024-02-02 15:31:58 +0100

    jsdoc

--- js/game.js
+++ js/game.js
@@ -6,6 +6,9 @@ let i = 1;
 let music = new Audio('audio/music.mp3');
 
 
+/**
+ * Init function, which initialises the game and thus starts it.
+ */
 function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
@@ -13,6 +16,9 @@ function init() {
 }
 
 
+/**
+ * Function to show or hide buttons to control the game.
+ */
 function handleShowableButtons() {
     document.getElementById('helpButton').classList.add('d-none');
     document.getElementById('startButton').classList.add('d-none');
@@ -25,6 +31,9 @@ function handleShowableButtons() {
 }
 
 
+/**
+ * Function for displaying a start image in the canvas container.
+ */
 function startImage() {
     let ctx = document.getElementById('canvas').getContext('2d');
     let img = new Image;
@@ -35,21 +44,33 @@ function startImage() {
 }
 
 
+/**
+ * Function for displaying a picture overlay when the game has been lost.
+ */
 function lostImage() {
     document.getElementById('lostImage').classList.remove('d-none');
 }
 
 
+/**
+ * Function for displaying a picture overlay when the game has been won.
+ */
 function wonImage() {
     document.getElementById('wonImage').classList.remove('d-none');
 }
 
 
+/**
+ * Function for displaying an image overlay with instructions on gameplay and controls.
+ */
 function showHelp() {
     document.getElementById('instructions').classList.toggle('d-flex');
 }
 
 
+/**
+ * Function for playing and adjusting the volume of the background music.
+ */
 function playMusic() {
     music.play();
     music.volume = 0.2;
@@ -57,11 +78,17 @@ function playMusic() {
 }
 
 
+/**
+ * Function to stop the background music.
+ */
 function stopMusic() {
     music.pause();
 }
 
 
+/**
+ * Function for displaying the game in full screen mode.
+ */
 function goFullScreen() {
     let canvasContainer = document.getElementById('container');
     if (canvasContainer.requestFullscreen) {
@@ -73,11 +100,13 @@ function goFullScreen() {
     } else if (canvasContainer.msRequestFullscreen) {
         canvasContainer.msRequestFullscreen();
     }
-    document.getElementById('fullScreenButton').classList.add('d-none');
-    document.getElementById('exitFullScreenButton').classList.remove('d-none');
+    toggleFullScreenButton();
 }
 
 
+/**
+ * Function to exit full screen mode.
+ */
 function exitFullScreen() {
     if (document.exitFullscreen) {
         document.exitFullscreen();
@@ -88,17 +117,35 @@ function exitFullScreen() {
     } else if (document.msExitFullscreen) {
         document.msExitFullscreen();
     }
-    document.getElementById('exitFullScreenButton').classList.add('d-none');
-    document.getElementById('fullScreenButton').classList.remove('d-none');
+    toggleFullScreenButton();
 }
 
 
+/**
+ * Function to change the appearance of the button for full screen mode.
+ */
+function toggleFullScreenButton() {
+    document.getElementById('fullScreenButton').classList.toggle('d-none');
+    document.getElementById('exitFullScreenButton').classList.toggle('d-none');
+}
+
+
+/**
+ * This function combines all setInterval functions into a new function collection called setStoppableInterval. 
+ * SetStoppableInterval is an array called intervalIds.
+ * 
+ * @param {value} fn - Value to pass the entire function within the setInterval function.
+ * @param {value} time - Value to pass the interval time.
+ */
 function setStoppableInterval(fn, time) {
     let id = setInterval(fn, time);
     intervalIds.push(id);
 }
 
 
+/**
+ * This function can be used to stop all setInterval functions that are stored with the intervalId in the intervalIds array.
+ */
 function stopGame() {
     intervalIds.forEach(clearInterval);
 }
