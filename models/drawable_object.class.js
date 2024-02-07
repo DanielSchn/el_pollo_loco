99e361ecf4c9a8135d566fef9f4fb1067444a0d3
DanielSchn <d.schneider@dsc-mail.de>
2024-02-07 16:34:50 +0100

    jsdoc

--- models/drawable_object.class.js
+++ models/drawable_object.class.js
@@ -8,17 +8,33 @@ class DrawableObject {
     currentImage = 0;
 
 
+    /**
+     * This function draws the images in the canvas container.
+     * 
+     * @param {Object} ctx - ctx is the object to draw into the canvas container.
+     */
     draw(ctx) {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
 
+    /**
+     * This function loads a new image and generates the path for further processing.
+     * 
+     * @param {string} path - The path to the image from the drawable objects.
+     */
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
 
+    /**
+     * This function draws a frame around objects. This is mainly useful for development.
+     * Only objects of the character, chickens, endboss, bottle and coin are taken into account.
+     * 
+     * @param {Object} ctx - ctx is the object to draw into the canvas container.
+     */
     drawFrame(ctx) {
         if (this instanceof Character || this instanceof Chicken || this instanceof Endboss || this instanceof Bottle || this instanceof Coin) {
             ctx.beginPath();
@@ -43,6 +59,9 @@ class DrawableObject {
     }
 
 
+    /**
+     * This function ends the game and then shows a won or lost overlay.
+     */
     stopAnimation() {
         setInterval(() => {
             if (this.percentage == 0) {
