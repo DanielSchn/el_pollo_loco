9bc3a1b5d342a95cc42285ef4c4ca0f59c0d5874
DanielSchn <d.schneider@dsc-mail.de>
2024-02-07 09:45:05 +0100

    correct chicken y axis

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,7 @@
 class Chicken extends MovableObject {
     height = 60;
     width = 62;
-    y = 375;
+    y = 360;
 
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
