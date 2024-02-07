14cc1a2fc2f36fe5b195e98841809025357d4f07
DanielSchn <d.schneider@dsc-mail.de>
2024-02-07 12:29:03 +0100

    endboss movement

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -6,28 +6,32 @@ class Endboss extends MovableObject {
     hadFirstContact = false;
 
     IMAGES_WALKING = [
-        'img/4_enemie_boss_chicken/2_alert/G5.png',
-        'img/4_enemie_boss_chicken/2_alert/G6.png',
-        'img/4_enemie_boss_chicken/2_alert/G7.png',
-        'img/4_enemie_boss_chicken/2_alert/G8.png',
-        'img/4_enemie_boss_chicken/2_alert/G9.png',
-        'img/4_enemie_boss_chicken/2_alert/G10.png',
-        'img/4_enemie_boss_chicken/2_alert/G11.png',
-        'img/4_enemie_boss_chicken/2_alert/G12.png'
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png',
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
     ];
-
     IMAGES_HURT = [
         'img/4_enemie_boss_chicken/4_hurt/G21.png',
         'img/4_enemie_boss_chicken/4_hurt/G22.png',
         'img/4_enemie_boss_chicken/4_hurt/G23.png'
     ];
-
     IMAGES_DEAD = [
         'img/4_enemie_boss_chicken/5_dead/G24.png',
         'img/4_enemie_boss_chicken/5_dead/G25.png',
         'img/4_enemie_boss_chicken/5_dead/G26.png'
     ];
-
     IMAGES_ATTACK = [
         'img/4_enemie_boss_chicken/3_attack/G13.png',
         'img/4_enemie_boss_chicken/3_attack/G14.png',
@@ -52,6 +56,7 @@ class Endboss extends MovableObject {
 
     animateEndboss() {
         setStoppableInterval(() => this.playEndboss(), 100);
+        setStoppableInterval(() => this.moveEndboss(), 1000 / 60);
     }
 
 
@@ -65,7 +70,8 @@ class Endboss extends MovableObject {
         } else {
             if (i < 20) {
                 this.playAnimation(this.IMAGES_WALKING);
-            } else {
+            } 
+            else {
                 this.playAnimation(this.IMAGES_ATTACK);
             }
             i++;
@@ -87,5 +93,13 @@ class Endboss extends MovableObject {
     endbossHurtAnimation() {
         this.playAnimation(this.IMAGES_HURT);
         this.animationPlayed = 0;
+    }
+
+
+    moveEndboss() {
+        if (this.isDead()) {
+        } else {
+            this.moveLeft(false);
+        }
     }
 }
